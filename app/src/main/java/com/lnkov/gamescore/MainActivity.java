98e7c63542d82836a082d1f7adcab1a811324fec
Alex Lenkov <alex.lenkov123@outlook.com>
2024-04-10 10:10:48 +0300

    add: logs, onStart onStop updateScore method

--- app/src/main/java/com/lnkov/gamescore/MainActivity.java
+++ app/src/main/java/com/lnkov/gamescore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lnkov.gamescore;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     private int score1 = 0;
     private int score2 = 0;
+    private TextView textViewPointTeam1;
+    private TextView textViewPointTeam2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,41 +30,70 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.d("MainActivity", "Method onCreate");
+
 
         if (savedInstanceState != null) {
             score1 = savedInstanceState.getInt("score1");
             score2 = savedInstanceState.getInt("score2");
         }
 
+        textViewPointTeam1 = findViewById(R.id.textViewPointTeam1);
+        textViewPointTeam2 = findViewById(R.id.textViewPointTeam2);
 
-        TextView textViewPointTeam1 = findViewById(R.id.textViewPointTeam1);
-        TextView textViewPointTeam2 = findViewById(R.id.textViewPointTeam2);
+        updateScore1();
+        updateScore2();
 
-        textViewPointTeam1.setText(String.valueOf(score1));
-        textViewPointTeam2.setText(String.valueOf(score2));
 
         textViewPointTeam1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewPointTeam1.setText(String.valueOf(++score1));
+                Log.d("MainActivity", "Clicked textViewScore1");
+                score1++;
+                updateScore1();
             }
         });
         textViewPointTeam2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textViewPointTeam2.setText(String.valueOf((++score2)));
+                Log.d("MainActivity", "Clicked textViewScore2");
+                score2++;
+                updateScore2();
             }
         });
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop");
+    }
+
+    private void updateScore1() {
+        textViewPointTeam1.setText(String.valueOf(score1));
+    }
+
+    private void updateScore2() {
+        textViewPointTeam2.setText(String.valueOf(score2));
+    }
+
+
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d("MainActivity", "Method onSaveInstanceState");
         super.onSaveInstanceState(outState);
         outState.putInt("score1", score1);
         outState.putInt("score2", score2);
     }
 
     protected void onDestroy() {
+        Log.d("MainActivity", "Method onDestroy");
         super.onDestroy();
     }
 }
